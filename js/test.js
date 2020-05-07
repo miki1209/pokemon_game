7205c741b1ad96830c64041563dfd2e93dcd1b13
koyama miki <64634979+miki1209@users.noreply.github.com>
2020-05-07 23:02:22 +0900

    ajax 追加

--- js/test.js
+++ js/test.js
@@ -1,16 +1,11 @@
 $(function(){
-        json = "../json/test.json";
-        target = $('#list');
-        $.getJSON(json, function(data,status){
-            for(var n in data) {
-                var text = '<li>';
-                if (data[n].url){
-                    line = '<a href="'+data[n].url+'" target="_blank"><span>'+data[n].name+'</span></a>';
-                }else{
-                    line = '<i><span>'+data[n].name+'</span></i>';
-                }
-                text = text+line+'</li>';
-                $(target).append(text);
-            }
-        });
+        $.ajax({
+        url: '../json/pokemon_list.json', //アクセスするURL
+        type: 'get',
+        contentType:'application/json',
+        //Access-Control-Allow-origin:*,
+        dataType:'json',     //data type script・xmlDocument・jsonなど
+      }).then(function(deta){
+        console.log(data);
+    })
     });
