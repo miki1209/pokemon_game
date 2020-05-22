ff7f5d8dca3774f8daba7ad717c13bbb70982e0a
koyama miki <64634979+miki1209@users.noreply.github.com>
2020-05-22 16:37:59 +0900

    testファイルを削除しました

--- js/index.js
+++ js/index.js
@@ -89,3 +89,50 @@ console.log(path);
 var player1IndividualId = path.split("?p1=")[1]
 //変数を定義しているためlocation.searchはpathになる
 
+// ここからajax
+
+// $(function(){
+//         $.ajax({
+//         url: '../json/pokemon.json', //アクセスするURL
+//         type: 'get',
+//         contentType:'application/json',
+//         //Access-Control-Allow-origin:*,
+//         dataType:'json',     //data type script・xmlDocument・jsonなど
+//       }).then(function(deta){
+//         console.log(deta);
+//     })
+// });
+
+// function ajax(url,type,dataType){
+//   return $.ajax({
+//     url: url,
+//     type: type,
+//     dataType: dataType
+//     })
+// }
+
+// window.onload = function() {
+//   ajax('json/pokemon.json','get','json')
+//   .done(function(data){
+//     console.log(data);
+//   }).fail(function(err){
+//     console.log(err);
+//   })
+// };
+
+$(function(){
+
+  $.ajax({
+    url: 'json/pokemon_list.json',
+    type: 'get',
+    dataType: 'json'
+  }).then(
+    function (data,dataType){
+      console.log(data);
+      for (var i = 0; data.length > i  ;i++){
+        console.log(i);}
+    },
+    function (){
+      
+    });
+});
